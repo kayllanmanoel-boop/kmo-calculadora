bca182c9756b0d21f810e4cb0e64c5ec9b1c076b
kayllanmanoel-boop <kayllan.manoel@gmail.com>
2026-09-02 23:42:53 -0300

    Adiciona carregador base de questões Feogia

---
+++ feogia/data-base.js
@@ -0,0 +1,2 @@
+const PHASES=[['🌎','América'],['🌍','África'],['🌏','Ásia'],['🌍','Europa'],['🌏','Oceania'],['🧊','Antártida'],['🌊','Oceanos'],['☀️','Climas'],['🌱','Vegetação'],['🏆','Desafio Final']];
+const BANKS={};
